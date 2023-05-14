a59b922efeea7f7b7911c2a6684d615f26483a47
Igainst <antimagdota2@mail.ru>
2023-05-14 18:39:28 +0300

    update

--- src/components/Content/constant.js
+++ src/components/Content/constant.js
@@ -10,30 +10,44 @@ export const PRODUCTS = [
     id: 1,
     name: "New Balance 564 Vintage Brights",
     img: "images/6.jpg",
-    price: 650,
+    price: "$ 800",
   },
   {
     id: 1,
     name: "New Balance 564 Vintage Brights",
     img: "images/11.jpg",
-    price: 650,
+    price: "$ 1000",
   },
   {
     id: 2,
     name: "New Balance 574 Vintage Brights",
     img: "images/0.jpg",
-    price: 650,
+    price: "$ 1200",
   },
   {
     id: 3,
     name: "New Balance Made in UK 920 Chinese New Year",
     img: "images/8.jpg",
-    price: 650,
+    price: "$ 1400",
   },
   {
     id: 4,
     name: "New Balance 373 Modern Classics",
     img: "images/2.jpg",
-    price: 650,
+    price: "$ 1500",
+  },
+];
+export const PRODUCTSBASKET = [
+  {
+    id: 1,
+    name: "New Balance 373 Modern Classics",
+    img: "images/11.jpg",
+    price: "$ 1500",
+  },
+  {
+    id: 2,
+    name: "New Balance 373 Modern Classics",
+    img: "images/12.jpg",
+    price: "$ 1500",
   },
 ];
