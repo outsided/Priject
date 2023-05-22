4332e52a28cf92bc91f9fb0b220573263e74a0cb
kosykh-a <aleksandr.kosykh@nedra.digital>
2023-05-22 19:58:58 +0300

    update

--- src/UI/Button/Button.jsx
+++ src/UI/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 
 const Button = (props) => {
     return (
-        <button className={styles.button} type="button" ><img className={props.styles.button} src={props.icon} alt={props.icon}></img></button>
+        <button className={styles.button} type="button" ><img className={styles.button} src={props.icon} alt={props.icon}></img></button>
     )
 }
 export default Button;
